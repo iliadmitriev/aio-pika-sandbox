0df121640a740972021b4fc681d1c09ebac6faf6
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-10 11:24:37 +0300

    aiohttp server which sends messages on get query

---
+++ app.py
@@ -0,0 +1,53 @@
+from aiohttp import web
+import asyncio
+from aio_pika import connect, Message
+
+
+async def init_pika(app):
+    loop = asyncio.get_event_loop()
+    rmq_url = app['rmq_url']
+    connection = await connect(
+        url=rmq_url, loop=loop
+    )
+
+    # Creating a channel
+    channel = await connection.channel()
+    app['connection'] = connection
+    app['channel'] = channel
+
+
+async def close_pika(app):
+    connection = app['connection']
+    await connection.close()
+
+
+def setup_pika(app, rmq_url):
+    app['rmq_url'] = rmq_url
+    app.on_startup.append(init_pika)
+    app.on_cleanup.append(close_pika)
+
+
+async def send_handler(request):
+    message = {'message': 'Hello world'}
+    channel = request.app['channel']
+    await channel.default_exchange.publish(
+        Message(b"Hello World!"),
+        routing_key="hello",
+    )
+    return web.json_response(message)
+
+
+async def init_app():
+    app = web.Application()
+    setup_pika(app, 'amqp://admin:adminsecret@192.168.10.1/hello')
+    app.router.add_route('GET', '/send', send_handler)
+    return app
+
+
+def main():
+    app = init_app()
+    web.run_app(app)
+
+
+if __name__ == "__main__":
+    main()
